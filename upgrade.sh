49025844ed83628e740003d1d313da68f01d7652
Eddie Zaneski <eddiezane@gmail.com>
2020-11-06 13:02:51 -0700

    Update for new network
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 # TODO: make this an actual script
 
-k3sup install --user pi --ip 192.168.1.10 --k3s-extra-args '--no-deploy servicelb' --k3s-version 'v1.19.3+k3s2'
-k3sup join --user pi --server-ip 192.168.1.10 --ip 192.168.1.11 --k3s-version 'v1.19.3+k3s2'
-k3sup join --user pi --server-ip 192.168.1.10 --ip 192.168.1.12 --k3s-version 'v1.19.3+k3s2'
+k3sup install --user pi --ip 192.168.78.10 --k3s-extra-args '--no-deploy servicelb' --k3s-version 'v1.19.3+k3s2'
+k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.11 --k3s-version 'v1.19.3+k3s2'
+k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.12 --k3s-version 'v1.19.3+k3s2'
