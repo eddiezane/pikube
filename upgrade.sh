3d72020f7ba8c31b86c732bb9655ad6da915118f
Eddie Zaneski <eddiezane@gmail.com>
2021-01-14 18:18:46 -0700

    Update ozwdaemon
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # TODO: make this an actual script
 
-k3sup install --user pi --ip 192.168.78.10 --k3s-extra-args '--no-deploy servicelb' --k3s-version 'v1.19.3+k3s2'
-k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.11 --k3s-version 'v1.19.3+k3s2'
-k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.12 --k3s-version 'v1.19.3+k3s2'
+k3sup install --user pi --ip 192.168.78.10 --k3s-extra-args '--no-deploy servicelb' --k3s-version 'v1.20.0+k3s2'
+k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.11 --k3s-version 'v1.20.0+k3s2'
+k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.12 --k3s-version 'v1.20.0+k3s2'
+k3sup join --user pi --server-ip 192.168.78.10 --ip 192.168.78.13 --k3s-version 'v1.20.0+k3s2'
